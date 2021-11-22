c5843a403c6b7eaee28a3c15ffec56ce24e5011a
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-11-22 21:19:12 +0700

    first commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Println("hit the handlers")
+
+}
---
+++ cmd/web/render.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+type templateData struct {
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	IsAuthenticated int
+	API             string
+	CSSVersion      string
+}
+
+var functions = template.FuncMap{}
+
+// Package embed provides access to files embedded in the running Go program.
+// Go source files that import "embed" can use the //go:embed directive to initialize a variable of type string, []byte, or FS with the contents of files read from the package directory or subdirectories at compile time.
+// allow us to compile the app into binary with all embedded file, very convenient
+
+//go:embed templates
+var templateFS embed.FS
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	// temporary
+	return td
+}
+
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+	templateToRender := fmt.Sprintf("template/%s.page.tmpl", page)
+
+	// if template exist in cache templeInMap will be true otherwise false
+	_, templateInMap := app.templeCache[templateToRender]
+
+	// we want to use the cache in production only
+	if app.config.env == "production" && templateInMap {
+		t = app.templeCache[templateToRender]
+	} else {
+		// build the template
+		t, err = app.parseTemplate(partials, page, templateToRender)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+
+	// so we check to see if templateData was passed with the call to ??? at defaultdata
+	// if it was not we create an empty templateData Object
+	if td == nil {
+		td = &templateData{}
+	}
+
+	// then we add our default data
+	td = app.addDefaultData(td, r)
+
+	// finally execute the template
+	err = t.Execute(w, td)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (app *application) parseTemplate(partials []string, page, templateToRender string) (*template.Template, error) {
+	var t *template.Template
+	var err error
+
+	// build the partials
+	// prepending templates to the partials name
+	if len(partials) > 0 {
+		for i, x := range partials {
+			partials[i] = fmt.Sprintf("templates/%s.partial.tmpl", x)
+		}
+	}
+
+	// if we do have partials we have to call our parseTemplate function
+	// .Funcs(functions) pass some functions available to our template
+	if len(partials) > 0 {
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", strings.Join(partials, ","), templateToRender)
+	} else {
+		// situation we don t have our partials associated with the template
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", templateToRender)
+	}
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return nil, err
+	}
+
+	// set in cache
+	app.templeCache[templateToRender] = t
+
+	return t, nil
+}
